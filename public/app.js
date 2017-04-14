1860550cc2ddee0e0183691c22093933c7e1cf8b
Alice Loudon <alice_loudon@icloud.com>
2017-04-14 13:33:47 +0100

    Do API call. Call getData() in app.js and console.log the data

--- public/app.js
+++ public/app.js
@@ -1,5 +1,10 @@
 var app = function(){
   var munroList = new MunroList("https://munroapi.herokuapp.com/api/munros")
+
+  munroList.getData(function(munros){
+    
+  })
+
 }
 
 window.onload = app
--- public/munro_list.js
+++ public/munro_list.js
@@ -1,4 +1,22 @@
 var MunroList = function(url){
   this.url = url
-  this.characters = []
+  this.munros = []
+}
+
+MunroList.prototype = {
+
+  getData: function(callback){
+    var request = new XMLHttpRequest()
+    request.open("GET", this.url)
+    request.onload = function(){
+      if (request.status === 200){
+        var jsonString = request.responseText
+        this.munros = JSON.parse(jsonString)
+        console.log(this.munros)
+        callback(this.munros)
+      }
+    }.bind(this)
+    request.send()
+  }
+
 }
