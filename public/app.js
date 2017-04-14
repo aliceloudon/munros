5327e8fe9192291fb46e9a204fc0dc5abd683d2e
Alice Loudon <alice_loudon@icloud.com>
2017-04-14 13:40:31 +0100

    Define new object, MunroSelectView

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
 var app = function(){
   var munroList = new MunroList("https://munroapi.herokuapp.com/api/munros")
+  var munroSelectView = new MunroSelectView(document.querySelector('#munros'))
 
   munroList.getData(function(munros){
-    
+
   })
 
 }
