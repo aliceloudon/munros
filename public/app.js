467be74f9e0d3e8c625667637eb4335d0ebe30dd
Alice Loudon <alice_loudon@icloud.com>
2017-04-14 13:55:57 +0100

    Make new object, MunroDetailsView

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 var app = function(){
   var munroList = new MunroList("https://munroapi.herokuapp.com/api/munros")
   var munroSelectView = new MunroSelectView(document.querySelector('#munros'))
+  var munroDetailsView = new MunroDetailsView(document.querySelector('#munro-details'))
 
   munroList.getData(function(munros){
     munroSelectView.render(munros)
---
+++ public/munro_details_view.js
@@ -0,0 +1,3 @@
+var MunroDetailsView = function(detailsElement){
+  this.detailsElement = detailsElement
+}
